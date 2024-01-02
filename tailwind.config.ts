00ee8aa4c01010753eab2ddc6a111e15a32581d7
T1CkShot <fatta.mmg@gmail.com>
2024-01-02 06:07:33 +0600

    remove unsed property

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,19 +7,16 @@ const config: Config = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
     colors: {
       text: "hsl(205, 49%, 93%)",
       background: "hsl(210, 50%, 7%)",
       primary: "hsl(211, 50%, 73%)",
       secondary: "hsl(261, 50%, 35%)",
       accent: "hsl(279, 50%, 54%)",
+    },
+    fontFamily: {
+      header: "var(--font-poppins)",
+      body: "var(--font-inter)",
     },
   },
   plugins: [],

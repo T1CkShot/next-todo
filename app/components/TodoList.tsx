02d2833dba938faaf0f86abd7de0225bde560954
T1CkShot <fatta.mmg@gmail.com>
2024-01-05 00:05:50 +0600

    style TodoList

--- app/components/TodoList.tsx
+++ app/components/TodoList.tsx
@@ -7,11 +7,14 @@ interface Props {
 export default function TodoList({ todos }: Props) {
   return (
     <>
-      <ul>
+      <ul className="w-full flex flex-col items-start mt-4 max-w-xl">
         {todos.map((todo) => {
           return (
-            <li key={todo.id}>
-              <input type="checkbox" />
+            <li key={todo.id} className="p-1 font-body text-xl gap-2 flex">
+              <input
+                type="checkbox"
+                className="w-6 text-primary align-middle accent-primary"
+              />
               {todo.name}
             </li>
           );

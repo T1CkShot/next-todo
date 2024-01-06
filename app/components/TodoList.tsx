dd8798b835848432c4fbf4b6cced390cbea922ef
T1CkShot <fatta.mmg@gmail.com>
2024-01-06 08:10:44 +0600

    add delete button

--- app/components/TodoList.tsx
+++ app/components/TodoList.tsx
@@ -1,22 +1,33 @@
-"use client";
-import type { Todo } from "@/lib/todos";
+import { Todo, deleteTodo } from "@/lib/todos";
+import { TrashIcon } from "@heroicons/react/24/solid";
+import { Dispatch, SetStateAction } from "react";
 
 interface Props {
   todos: Todo[] | null;
+  setTodos: Dispatch<SetStateAction<Todo[] | null>>;
 }
 
-export default function TodoList({ todos }: Props) {
+export default function TodoList({ todos, setTodos }: Props) {
   return (
     <>
-      <ul className="w-full flex flex-col items-start mt-4 max-w-xl">
+      <ul className="w-full flex flex-col items-start mt-5 max-w-xl">
         {todos?.map((todo) => {
           return (
-            <li key={todo.id} className="p-1 font-body text-xl gap-2 flex">
+            <li
+              key={todo.id}
+              className="p-1 font-body text-xl gap-2 flex w-full"
+            >
               <input
                 type="checkbox"
                 className="w-6 text-primary align-middle accent-primary"
               />
               {todo.name}
+              <button
+                className="p-2 font-bold hover:bg-primary flex items-center rounded-lg ml-auto"
+                onClick={() => deleteTodo(todo.id, setTodos)}
+              >
+                <TrashIcon className="h-4 w-4" />
+              </button>
             </li>
           );
         })}
